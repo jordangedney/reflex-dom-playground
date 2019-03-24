7e6cc94b509f433966474a7323664533076e8724
Jordan Gedney <jordangedney@gmail.com>
2019-03-23 23:03:12 -0400

    level up nix

--- default.nix
+++ default.nix
@@ -4,15 +4,10 @@
 with import ./.obelisk/impl { inherit system iosSdkVersion; };
 project ./. ({ pkgs, ... }: {
   overrides = self: super: let
-    reflexDomCanvas = pkgs.fetchFromGitHub {
-      owner = "qfpl";
-      repo = "reflex-dom-canvas";
-      rev = "aa1cf4c83dbcab1e59e5b607abf13497e912cd81";
-      sha256 = "1r4fdj02qljy4b8pv05law4jkrgwdpi6l36zmm4951410hkxpppa";
-    };
+    reflex-dom-canvas = import ./nix/reflex-dom-canvas.nix;
   in
   {
-    reflex-dom-canvas = self.callCabal2nix "reflex-dom-canvas" reflexDomCanvas {};
+    reflex-dom-canvas = self.callCabal2nix "reflex-dom-canvas" reflex-dom-canvas {};
   };
 
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
---
+++ nix/update.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+nix-prefetch-git https://github.com/qfpl/reflex-dom-canvas > reflex-dom-canvas.json
